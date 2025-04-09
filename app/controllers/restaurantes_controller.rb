4deaf75970558ad407c6d1fc6c87898b4115637e
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-08 22:19:15 -0300

    exibe 3 restaurantes por página

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -3,7 +3,6 @@ class RestaurantesController < ApplicationController
 
   # GET /restaurantes or /restaurantes.json
   def index
-    # @restaurantes = Restaurante.all.order(created_at: :desc)
     # Exibe 3 restaurantes por página. Você pode ajustar a quantidade conforme necessário.
     @restaurantes = Restaurante.page(params[:page]).per(3)
   end
